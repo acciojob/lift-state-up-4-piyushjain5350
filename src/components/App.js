4e970f2909740f14b4f807cbdef82aa73dfc957c
Piyush Jain <piyushjain5350@gmail.com>
2023-08-15 13:16:20 +0530

    3 solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,98 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-import Child from "./Child";
 
 const App = () => {
-  let [name,setName]=useState('');
-  let [price,setPrice]=useState(0);
-  let [list,setList]=useState([]);
-
-  function handlingFn(){
-    let obj={
-      name:name,
-      price:price
-    };
-
-    setList([...list,obj]);
-    // console.log(list);
+  let [itemName , setItemName] = useState("");
+  let [itemPrice , setItemPrice] = useState("");
+  let [list,setList] = useState([{'ItemName' : "Item 1" , 'ItemPrice' : "$10",},{'ItemName' : "Item 2" , 'ItemPrice' : "$20",},{'ItemName' : "Item 3" , 'ItemPrice' : "$30",}])
+  function handleItemName(e){
+    setItemName(e.target.value)
   }
-  
-  const handleRemoveItem = (index) => {
-    const updatedList = list.filter((item, i) => i !== index);
-    setList(updatedList);
-  };
-  
+function handleItemPrice(e){
+  setItemPrice(e.target.value)
+}
+function addItemsToList(){
+  if(itemName && itemPrice){
+    const newItem = {'ItemName': itemName, 'ItemPrice': `$${itemPrice}`};
+    setList([...list,newItem]);
+    setItemName("");
+    setItemPrice("");
+  }
+}
+function removeItem(index){
+  const updatedList = list.filter((item, idx) => idx !== index);
+  setList(updatedList);
+}
   return (
     <div className="parent">
-        {/* Do not remove the main div */}
-        <h1>Parent Component</h1>
-        <div className="input-container">
-          <label htmlFor="nameInput">Item Name</label>
-          <input type="text" name="nameInput" id="itemName" onChange={(e)=>setName(e.target.value)}/>
-          <label htmlFor="price">Item Price</label>
-          <input type="text" name="price" id="itemPrice" onChange={(e)=>setPrice(e.target.value)}/>
-          
-          <button onClick={handlingFn} >Add Item</button>
-
-          <Child list={list} onRemoveItem={handleRemoveItem}/>
-        </div>
+      <h1>Parent Component</h1>
+      <label htmlFor="itemName">Item Name:<input type="text" value= {itemName} onChange={handleItemName} id="itemName"/>
+      </label>
+      <label htmlFor="itemPrice">Item Price:<input type="text" value= {itemPrice} onChange={handleItemPrice} id="itemPrice"/>
+      </label>
+       <button onClick={addItemsToList}>Add Items</button>
+      <div className="child">
+        <h2>Child Component</h2>
+        <ul>
+         {
+          list.map((item,idx) => {
+           return (
+           <li key={idx}>{item.ItemName} - {item.ItemPrice} 
+               <button onClick={() => removeItem(idx)}>
+               Remove
+              </button>
+            </li>)
+         })
+         }
+         </ul>
+      </div>
     </div>
   )
 }
 
 export default App
+
+
+// import React, {useState} from "react";
+// import './../styles/App.css';
+// import Child from "./Child";
+
+// const App = () => {
+//   let [name,setName]=useState('');
+//   let [price,setPrice]=useState(0);
+//   let [list,setList]=useState([]);
+
+//   function handlingFn(){
+//     let obj={
+//       name:name,
+//       price:price
+//     };
+
+//     setList([...list,obj]);
+//     // console.log(list);
+//   }
+  
+//   const handleRemoveItem = (index) => {
+//     const updatedList = list.filter((item, i) => i !== index);
+//     setList(updatedList);
+//   };
+  
+//   return (
+//     <div className="parent">
+//         {/* Do not remove the main div */}
+//         <h1>Parent Component</h1>
+//         <div className="input-container">
+//           <label htmlFor="nameInput">Item Name</label>
+//           <input type="text" name="nameInput" id="itemName" onChange={(e)=>setName(e.target.value)}/>
+//           <label htmlFor="price">Item Price</label>
+//           <input type="text" name="price" id="itemPrice" onChange={(e)=>setPrice(e.target.value)}/>
+          
+//           <button onClick={handlingFn} >Add Item</button>
+
+//           <Child list={list} onRemoveItem={handleRemoveItem}/>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default App
--- src/components/Child.js
+++ src/components/Child.js
@@ -1,36 +1,36 @@
+// // import React from "react";
+
+// // function Child({list}) {
+// //     return(
+// //         <div className="child">
+// //             <h1>Child Component</h1>
+// //             <ul className="listItem">
+// //                 {/* <li>
+// //                     Item 1-$15 <button>Remove</button>
+// //                 </li> */}
+// //                 console.log(list);
+// //             </ul>
+// //         </div>
+// //     )
+// // }
+
+// // export default Child;
 // import React from "react";
 
-// function Child({list}) {
-//     return(
-//         <div className="child">
-//             <h1>Child Component</h1>
-//             <ul className="listItem">
-//                 {/* <li>
-//                     Item 1-$15 <button>Remove</button>
-//                 </li> */}
-//                 console.log(list);
-//             </ul>
-//         </div>
-//     )
+// function Child({ list ,onRemoveItem }) {
+//   return (
+//     <div className="child">
+//       <h1>Child Component</h1>
+//       <ul className="listItem">
+//         {list.map((item, index) => (
+//           <li key={index}>
+//             {item.name} - ${item.price}
+//             <button onClick={() => onRemoveItem(index)}>Remove</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
 // }
 
 // export default Child;
-import React from "react";
-
-function Child({ list ,onRemoveItem }) {
-  return (
-    <div className="child">
-      <h1>Child Component</h1>
-      {/* <ul className="listItem"> */}
-        {list.map((item, index) => (
-          <li key={index}>
-            {item.name} - ${item.price}
-            <button onClick={() => onRemoveItem(index)}>Remove</button>
-          </li>
-        ))}
-      {/* </ul> */}
-    </div>
-  );
-}
-
-export default Child;
